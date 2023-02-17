08c74c8cd55074f78032b934dd83b635f0a5f4fe
iyaz07 <moseseniade@gmail.com>
2023-02-17 12:55:16 -0800

    Learn If else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
 /**
- * main = main function for couunting randoms
- * what the hell does betty want
+ * main - main function for couunting randoms
  * Return: 0 if successful
- *
  */
 int main(void)
 {
@@ -15,12 +11,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes here*/
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
-	else if (n > 0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	return (0);
 }
