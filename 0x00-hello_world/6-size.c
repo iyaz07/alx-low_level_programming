e7da63fbb4fb5a8abeab4c5e4bd9c91d38ff3778
iyaz07 <moseseniade@gmail.com>
2023-02-16 17:02:36 -0800

    Learning C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("size of a char : %d\n"  sizeof(char));
+	printf("size of an Int : %d\n" sizeof(int));
+	printf("size of long Int : %d\n" sizeof(long));
+	printf("size of a long long Int : %d\n" sizeof(double));
+	printf("size of float : %d\n" sizeof(float));
+	return (0);
+}
