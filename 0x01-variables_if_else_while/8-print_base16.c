e387d223ec6600c8c5018cccf2db6117dcc2abca
iyaz07 <moseseniade@gmail.com>
2023-02-17 19:57:18 -0800

    Learning If, if else... C

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Main - A program to print the list of hex number
+ * Return: Return 0 if successful
+ */
+int main(void)
+{
+	int n;
+	char alph;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	for (alph = 'a'; alph <= 'f'; alph++)
+		putchar(alph);
+	putchar('\n');
+
+	return (0);
+}
