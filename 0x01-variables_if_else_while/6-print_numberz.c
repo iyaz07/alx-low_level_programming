8ea1e53bf26127fa273b9a7264bdb13f7bc58fec
iyaz07 <moseseniade@gmail.com>
2023-02-17 18:31:13 -0800

    Learning If, If else...C

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print all numbers between 0 and 10
+ * Return:  0 (If successful)
+ */
+int main(void)
+{
+	int Num;
+
+	for (Num = '0'; Num <= '9'; Num++)
+	putchar(Num);
+	
+	putchar('\n');
+	return (0);
+}
