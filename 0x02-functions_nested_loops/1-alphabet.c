2b244856059cdce9b55d0c60e0e390eee726aa2b
iyaz07 <moseseniade@gmail.com>
2023-02-21 13:27:46 -0800

    Learning functions, loops in C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_alphabet - check the code
+*
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+	char Alpha;
+	
+	for (Alpha ='a'; Alpha <= 'z'; Alpha++)
+		_putchar(Alpha);
+	_putchar('\n');
+}
