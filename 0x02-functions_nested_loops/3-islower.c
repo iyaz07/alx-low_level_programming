1c3dd0bc0a6eb0113dd39ae23d45fcf9c9310b8f
iyaz07 <moseseniade@gmail.com>
2023-02-21 16:29:38 -0800

    Learning functions, loops in C

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _islower - check the code.
+* @c: The character to be checked
+* Return: return 1 for lowercase or 0 for anything else.
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
