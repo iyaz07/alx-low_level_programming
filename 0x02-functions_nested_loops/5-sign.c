71145a0bb98393461c2f37f2aa50fdff361b99fd
iyaz07 <moseseniade@gmail.com>
2023-02-21 18:36:20 -0800

    Learning functions, loops in C

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_sign - Checks and prints
+*@n: the variable
+*Return: returns 1 if > 0. 0 if = 0. -1 if = 1;
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n > 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
