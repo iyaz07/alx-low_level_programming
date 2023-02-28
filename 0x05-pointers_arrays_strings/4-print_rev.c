41a35ddf628330b40209041a6502337e67708b83
iyaz07 <moseseniade@gmail.com>
2023-02-27 17:24:26 -0800

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,22 @@
 #include "main.h"
  #include <stdio.h>
 /**
- * swap_int - swaps two different pointer parameters
- * @a: first param
- * @b: second param
+ * print_rev - prints in revers
+ * @s: arg
  * Return: return is void
  */
 void print_rev(char *s)
 {
 	int i = 0;
 
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
 
 	for (; i >= 0; i--)
 	{
-		_putchar(s[i]);
+		fprintf(stderr, "%c", s[i]);
 	}
 	_putchar('\n');
 }
