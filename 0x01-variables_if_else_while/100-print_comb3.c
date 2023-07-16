60c818bcacf57170a989068e925e4faac76621b5
Iyaz07 <moseseniade@gmail.com>
2023-07-16 13:42:24 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * main prints hex
+  *
+  * Return: Always 0 (success)
+  */
+int main(void)
+{
+	int i = 0;
+	int j = 1;
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			if (j != i)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+		
+					if ((i == 8) && j == 9)
+						continue;
+                	        	putchar(',');
+                       			putchar(' ');
+				}
+                }
+        }
+        putchar('\n');
+        return (0);
+}
