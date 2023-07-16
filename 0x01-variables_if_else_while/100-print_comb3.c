b56212692d1407c8f11332edb6b60de011a8f333
Iyaz07 <moseseniade@gmail.com>
2023-07-16 13:48:28 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,22 +11,23 @@ int main(void)
 {
 	int i = 0;
 	int j = 1;
+
 	for (i = 0; i < 9; i++)
 	{
 		for (j = i + 1; j <= 9; j++)
 		{
 			if (j != i)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-		
-					if ((i == 8) && j == 9)
-						continue;
-                	        	putchar(',');
-                       			putchar(' ');
-				}
-                }
-        }
-        putchar('\n');
-        return (0);
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+
+				if ((i == 8) && j == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
