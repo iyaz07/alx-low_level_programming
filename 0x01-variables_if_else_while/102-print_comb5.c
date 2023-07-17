218d1ad0265759e29821e66a340ce2a86309b520
Iyaz07 <moseseniade@gmail.com>
2023-07-17 07:22:50 -0700

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * main prints double digits
+  *
+  * Return: Always 0 (success)
+  */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 98; i++)
+	{
+		for (j = i + 1; j < 99; j++)
+		{
+			{
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+				putchar(' ');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+
+				if ((i < 98) && j < 99)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
