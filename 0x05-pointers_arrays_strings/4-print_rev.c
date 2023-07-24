51e96c86d238a5dcdf1b72ae480a2327f3385eff
Iyaz07 <moseseniade@gmail.com>
2023-07-24 06:48:47 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - writes the character c to stdout
+ * @s: The string to reverse
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int o;
+
+	while(*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	s--;
+	for (o = i; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
