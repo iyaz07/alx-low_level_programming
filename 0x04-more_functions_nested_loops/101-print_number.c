1d6019ccd17fae2f637753d2312399d481a8c8f9
Iyaz07 <moseseniade@gmail.com>
2023-07-31 06:59:31 -0700

    Add pending

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,17 +8,17 @@ void print_integer(int m);
  */
 void print_number(int n)
 {
-	// If number is smaller than 0, put a - sign
-	// and change number to positive
+	/*If number is smaller than 0, put a - sign
+	 and change number to positive*/
 	if (n < 0) {
         	_putchar('-');
         	n = -n;
 	}
 
-	// Remove the last digit and recur
+	 /*Remove the last digit and recur*/
 	if (n/10)
-		print(n/10);
+		printf(n/10);
 
-	// Print the last digit
+	/*Print the last digit*/
 	_putchar(n%10 + '0');
 }
