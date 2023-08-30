1d45b5f2475cc5f7655ba175ebaa111e2f8dfdb9
Iyaz07 <moseseniade@gmail.com>
2023-08-30 06:01:15 -0700

    Update Palindrome Function

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 /**
+ * isPalindromeRecursive - Helper func
+ * @str: Parameter to be checked
+ * @start: first index
+ * @end: last index
+ * Return: 0 if palindrome and 1 otherwise
+ */
+
+int isPalindromeRecursive(char *str, int start, int end)
+{
+	if (start >= end)
+	{
+		return (1);  /*Base case: All characters have been compared*/
+	}
+	if (str[start] != str[end])
+	{
+		return (0);  /*Characters at symmetric positions are different*/
+	}
+	return (isPalindromeRecursive(str, start + 1, end - 1));
+}
+
+/**
  * is_palindrome - checks if a word spell the same forward and backward.
  * @str: Parameter to be checked
  * Return: 0 if palindrome and 1 otherwise
  */
 
-int is_palindrome(const char *str)
+int is_palindrome(char *str)
 {
 	int len = strlen(str);
 
-	for (int i = 0; i < len / 2; i++)
-	{
-		if (str[i] != str[len - i - 1])
-		{
-			return (0);/* Not a palindrome*/
-		}
-	}
-	return (1);/*Palindrome*/
+	return (isPalindromeRecursive(str, 0, len - 1));
 }
+
