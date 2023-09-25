319979443a35c30a992b68da0fb39e7fc97f3c5e
iyaz07 <moseseniade@gmail.com>
2023-09-25 11:19:33 -0700

    Fix Task 0and Push Task 1 to Print number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * listint_len - A function that prints all the elements in the list
+ * @h: A pointer to listint structure
+ * Return: the number of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		node_count++;
+		h = h->next;
+	}
+
+	return(node_count);
+}
+
