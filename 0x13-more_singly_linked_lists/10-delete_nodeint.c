54fee3691193795c1e734612c31f7abcf768b976
iyaz07 <moseseniade@gmail.com>
2023-09-26 07:39:39 -0700

    Add Task 9& 10 || made few corrections on previous Tasks

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ *  delete_nodeint_at_index -  deletes the node at index
+ *  index of a listint_t linked list
+ *
+ *  @head: head of the list
+ *  @index: index of the node to be deleted
+ *
+ *  Return: 1 on success, -1 if failed
+ *
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+
+	if (*head == NULL)
+	return (-1);  /*Empty list, deletion failed*/
+
+	listint_t *temp = *head;
+
+	/*Case 1: Delete the head node*/
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return (1);  /*Deletion succeeded*/
+	}
+
+    /*Traverse to the node before the desired position*/
+	for (i = 0; i < index - 1; i++)
+	{
+		if (temp == NULL || temp->next == NULL)
+		return (-1);  /*Index out of range, deletion failed*/
+		temp = temp->next;
+	}
+
+	listint_t *node_to_delete = temp->next;
+
+    /*Case 2: Delete at a valid position*/
+	if (node_to_delete != NULL)
+	{
+		temp->next = node_to_delete->next;
+		free(node_to_delete);
+		return (1);  /*Deletion succeeded*/
+	}
+
+	return (-1);  /*Index out of range, deletion failed*/
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ *
+ * @head: pointer to the head of the list
+ * @idx: index of the list where the new node should be added
+ * @n: element to be added
+ *
+ * Return: returns address of new node, or NULL if failure
+ *
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *current;
+	unsigned int i = 0;
+
+	if (new_node == NULL)
+		return NULL;
+
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	current = *head;
+
+	for (i = 0; i < idx - 1; i++)
+	{
+		if (current == NULL)
+		{
+			free(new_node);
+			return (NULL);  /*Index out of range*/
+       		}
+		current = current->next;
+	}
+
+	/*Case 2: Insert at a valid position*/
+	if (current != NULL)
+	{
+		new_node->next = current->next;
+		current->next = new_node;
+		return (new_node);
+	}
+
+	/*Case 3: Index out of range*/
+	free(new_node);
+	return (NULL);
+}
