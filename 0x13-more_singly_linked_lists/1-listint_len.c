04c923fa3fc3335f4b1615c33a4d93e9b6632ab0
iyaz07 <moseseniade@gmail.com>
2023-09-25 23:28:47 -0700

    Add free_list and fix errors on prev tasks

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -15,6 +15,6 @@ size_t listint_len(const listint_t *h)
 		h = h->next;
 	}
 
-	return(node_count);
+	return (node_count);
 }
 
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint -  frees a listint_t list
+ *
+ * @head: pointer to head of the list
+ *
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
+
