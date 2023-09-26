4ec50b9d79b5481bf05cdbc59f21eb25e85461e5
iyaz07 <moseseniade@gmail.com>
2023-09-26 07:50:43 -0700

    Fix betty on Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->n = n;
 
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			free(new_node);
 			return (NULL);  /*Index out of range*/
-       		}
+		}
 		current = current->next;
 	}
 
