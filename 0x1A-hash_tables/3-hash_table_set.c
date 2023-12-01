ef43475e40e1b06b66668dad6c69f5b5c4a0fa9c
iyaz07 <moseseniade@gmail.com>
2023-12-01 05:52:48 -0800

    Add: Whats wrong

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Inputs node in hash tables
+ * @ht: hash table to be used
+ * @key: key to be used
+ * @value: value at key
+ *
+ * Return 1, else 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node, *tmp;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL)
+		return (0);
+	
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(key, tmp->key) == 0)
+		{
+			tmp->value = strdup(value);
+			return (1);	
+		}
+		tmp = tmp->next;
+	}
+	node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = ht->array[index];
+	ht->array[index] = node;
+
+	return (1);
+}
+	
