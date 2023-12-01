31e943823d0682fd4a5f2c371d4a7d421cba2193
iyaz07 <moseseniade@gmail.com>
2023-12-01 06:53:54 -0800

    create hash table again

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,25 +8,25 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_t;
-	unsigned long int i;
+        hash_table_t *new_t;
+        unsigned long int i;
 
-	if (size == 0)
-		return (NULL);
-	new_t = malloc(sizeof(hash_table_t));
-	if (new_t == NULL)
-		return (NULL);
+        if (size == 0)
+                return (NULL);
+        new_t = malloc(sizeof(hash_table_t));
+        if (new_t == NULL)
+                return (NULL);
 
-	new_t->size = size;
+        new_t->size = size;
+        -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-hash_table_create.c -o a
+        new_t->array = malloc(sizeof(hash_node_t *) * size);
 
-	new_t->array = malloc(sizeof(hash_node_t *) * size);
-
-	if (new_t->array == NULL)
-	{
-		free(new_t);
-		return (NULL);
-	}
-	for (i = 0; i < size; i++)
-		new_t->array[i] = NULL;
-	return (new_t);
+        if (new_t->array == NULL)
+        {
+                free(new_t);
+                return (NULL);
+        }
+        for (i = 0; i < size; i++)
+                new_t->array[i] = NULL;
+        return (new_t);
 }
