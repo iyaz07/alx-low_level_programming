a4886b6ee8208b2d3669e3ea75286739510164ec
iyaz07 <moseseniade@gmail.com>
2023-12-03 00:27:37 -0800

    Fix the error finally

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node, *tmp;
 	unsigned long int index;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
@@ -22,6 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(key, tmp->key) == 0)
 		{
+			free(tmp->value);
 			tmp->value = strdup(value);
 			return (1);
 		}
