0fa43e57ab07c5d152aebed0dd0e2c4ca6422821
iyaz07 <moseseniade@gmail.com>
2024-03-12 10:18:51 -0700

    Check error

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -29,4 +29,24 @@ int binary_search(int *array, size_t size, int value)
             low = mid + 1;
     }
     return (-1);
-}			
+}
+#include "search_algos.h"
+/**
+ * print_array - prints a given array
+ * @start: printing starting point
+ * @stop: stopping point for printing
+ *
+ * Return: nothing
+ */
+void print_array(int *array, int start, int stop)
+{
+        int i;
+
+        if (array)
+        {
+                for (i = start; i < stop; i++)
+                        printf("%d, ", array[i]);
+                printf("%d\n", array[i]);
+        }
+        return;
+}
